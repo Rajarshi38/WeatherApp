0ebdf18ac5ab51b3b6885c6cf4d3b7f07a4d7de1
Rajarshi38 <rajarshibiswas38@gmail.com>
2021-07-05 22:11:13 +0530

    First upload

---
+++ script/app.js
@@ -0,0 +1,59 @@
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  console.log(data);
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  //update details template
+
+  details.innerHTML = `
+  <h5 class='my-3'>${cityDetails.EnglishName}</h5>
+  <h6 class="my-3">${weather.WeatherText}</h6>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  `;
+
+  const iconSrc = `./img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "./img/day.svg";
+  } else {
+    timeSrc = "./img/night.svg";
+  }
+  time.setAttribute("src", timeSrc);
+
+  //remove the d-none class
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+  return {
+    cityDetails: cityDetails,
+    weather: weather,
+  };
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = form.city.value.trim();
+
+  form.reset();
+
+  updateCity(city)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => console.log(err));
+});
---
+++ script/index.js
@@ -0,0 +1,30 @@
+const key = "k2tQTDxteGqWrAMYmYB5agoP2O3K6xp8";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// getCity("Kolkata")
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
