b33da5a90dc48cda5a895a0727fbd4380587bbf4
Rajarshi Biswas <72081132+Rajarshi38@users.noreply.github.com>
2021-08-14 10:16:35 +0530

    convert http to https

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ const key = "k2tQTDxteGqWrAMYmYB5agoP2O3K6xp8";
 
 //get weather information
 const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -12,7 +12,7 @@ const getWeather = async (id) => {
 
 //get city information
 const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
